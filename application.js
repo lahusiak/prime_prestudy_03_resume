31f0209440c24a063b90ac471b5766957e32be6f
lahusiak <lahusiak@gmail.com>
2015-10-11 20:52:02 -0500

    Initial commit

---
+++ application.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $(".main-content").hide();
+    });
+    $("#show").click(function(){
+        $(".main-content").show();
+    });
+});
